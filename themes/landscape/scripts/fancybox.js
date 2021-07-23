2b0f4c1aa490ed828e035cecaa25ee7ae1ff0dfd
Bo-Xuan Fan <coffee0127@gmail.com>
2021-07-23 23:21:11 +0800

    Update landscape theme

--- themes/landscape/scripts/fancybox.js
+++ themes/landscape/scripts/fancybox.js
@@ -11,14 +11,14 @@ hexo.extend.tag.register('fancybox', function(args){
   var original = args.shift(),
     thumbnail = '';
 
-  if (args.length && rUrl.test(args[0])){
+  if (args.length && rUrl.test(args[0])) {
     thumbnail = args.shift();
   }
 
   var title = args.join(' ');
 
-  return '<a class="fancybox" href="' + original + '" title="' + title + '">' +
-    '<img src="' + (thumbnail || original) + '" alt="' + title + '">'
-    '</a>' +
-    (title ? '<span class="caption">' + title + '</span>' : '');
+  return `<a data-fancybox="gallery" href="${original}" data-caption="${title}">
+    <img src="${thumbnail || original}" alt="${title}">
+    </a>
+    ${title ? `<span class="caption">${title}</span>` : ''}`;
 });
